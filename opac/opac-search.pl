c2bead5be5bfe914b95fefdbf83491fb9f3f625c
Paul Poulain <paul.poulain@biblibre.com>
2010-12-14 23:42:50 +0100

    MT4316 AdvancedSearchContent new feature (OPACAdvancedSearchContent)
    if those sysprefs are filled, the index list will be replaced by the content of the syspref
    
    That let the library define his own index list, with more or less lines on the list

--- opac/opac-search.pl
+++ opac/opac-search.pl
@@ -246,6 +246,10 @@ if ( $template_type && $template_type eq 'advsearch' ) {
 	if ( C4::Context->preference("expandedSearchOption") == 1 ) {
 		$template->param( expanded_options => C4::Context->preference("expandedSearchOption") );
 	}
+    if ( C4::Context->preference("OpacAdvancedSearchContent") ne '' ) {
+        $template->param( OpacAdvancedSearchContent => C4::Context->preference("OpacAdvancedSearchContent") );
+    }
+
 	# but let the user override it
 	if (defined $cgi->param('expanded_options')) {
    	    if ( ($cgi->param('expanded_options') == 0) || ($cgi->param('expanded_options') == 1 ) ) {
@@ -273,7 +277,7 @@ my @sort_by;
 my $default_sort_by = C4::Context->preference('OPACdefaultSortField')."_".C4::Context->preference('OPACdefaultSortOrder') 
     if (C4::Context->preference('OPACdefaultSortField') && C4::Context->preference('OPACdefaultSortOrder'));
 
-@sort_by = split("\0",$params->{'sort_by'}) if $params->{'sort_by'};
+@sort_by = $cgi->param('sort_by');
 $sort_by[0] = $default_sort_by if !$sort_by[0] && defined($default_sort_by);
 foreach my $sort (@sort_by) {
     $template->param($sort => 1);   # FIXME: security hole.  can set any TMPL_VAR here
@@ -281,8 +285,7 @@ foreach my $sort (@sort_by) {
 $template->param('sort_by' => $sort_by[0]);
 
 # Use the servers defined, or just search our local catalog(default)
-my @servers;
-@servers = split("\0",$params->{'server'}) if $params->{'server'};
+my @servers = $cgi->param('server');
 unless (@servers) {
     #FIXME: this should be handled using Context.pm
     @servers = ("biblioserver");
@@ -291,21 +294,18 @@ unless (@servers) {
 
 # operators include boolean and proximity operators and are used
 # to evaluate multiple operands
-my @operators;
-@operators = split("\0",$params->{'op'}) if $params->{'op'};
+my @operators = $cgi->param('op');
 
 # indexes are query qualifiers, like 'title', 'author', etc. They
-# can be single or multiple parameters separated by comma: kw,right-Truncation 
-my @indexes;
-@indexes = split("\0",$params->{'idx'}) if $params->{'idx'};
+# can be single or multiple parameters separated by comma: kw,right-Truncation
+my @indexes = $cgi->param('idx');
 
 # if a simple index (only one)  display the index used in the top search box
 if ($indexes[0] && !$indexes[1]) {
     $template->param("ms_".$indexes[0] => 1);
 }
 # an operand can be a single term, a phrase, or a complete ccl query
-my @operands;
-@operands = split("\0",$params->{'q'}) if $params->{'q'};
+my @operands = $cgi->param('q');
 
 # if a simple search, display the value in the search box
 if ($operands[0] && !$operands[1]) {
@@ -313,8 +313,7 @@ if ($operands[0] && !$operands[1]) {
 }
 
 # limits are use to limit to results to a pre-defined category such as branch or language
-my @limits;
-@limits = split("\0",$params->{'limit'}) if $params->{'limit'};
+my @limits = $cgi->param('limit');
 
 if($params->{'multibranchlimit'}) {
 push @limits, join(" or ", map { "branch: $_ "}  @{GetBranchesInCategory($params->{'multibranchlimit'})}) ;
