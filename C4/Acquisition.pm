dfc70ce1c61416dbd6010a0d0375777942159cb8
Paul Poulain <paul.poulain@biblibre.com>
2010-12-15 20:27:24 +0100

    Order cancelling improvement : delete attached items & biblio
    - all items attached to the order are deleted
    - if there is no more items, then the biblio is deleted as well

--- C4/Acquisition.pm
+++ C4/Acquisition.pm
@@ -1219,6 +1219,9 @@ sub DelOrder {
     my $sth = $dbh->prepare($query);
     $sth->execute( $bibnum, $ordernumber );
     $sth->finish;
+    my @itemnumbers = GetItemnumbersFromOrder( $ordernumber );
+    C4::Items::DelItem( $dbh, $bibnum, $_ ) for @itemnumbers;
+    DelBiblio(($bibnum)) if C4::Items::GetItemsCount( $bibnum ) == 0;
 }
 
 =head2 FUNCTIONS ABOUT PARCELS
