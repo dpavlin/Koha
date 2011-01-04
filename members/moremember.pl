0fdc7d5af46ee45bfb124ad9a68b42a71045665c
Paul Poulain <paul.poulain@biblibre.com>
2011-01-04 21:28:10 +0100

    removing unconditional warn chris c mail 2010-12-16 on patches ML

--- members/moremember.pl
+++ members/moremember.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 
 # Copyright 2000-2002 Katipo Communications
+# Copyright 2010 BibLibre
 #
 # This file is part of Koha.
 #
@@ -210,7 +211,6 @@ if ( $category_type eq 'A' || $category_type eq 'I') {
             }
         );
     }
-    warn Data::Dumper::Dumper(@guaranteedata);
     $template->param( guaranteeloop => \@guaranteedata );
     ( $template->param( adultborrower => 1 ) ) if ( $category_type eq 'A' || $category_type eq 'I' );
 }
