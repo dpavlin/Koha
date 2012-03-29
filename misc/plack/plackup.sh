d5e6336b398a63b4549c74e2d513934adb57bbee
Dobrica Pavlinusic <dpavlin@rot13.org>
2012-03-29 12:27:05 +0200

    Bug 7844 - plack intranet tooling for developers
    koha.psgi example and plackup.sh script to run any Koha site
    intranet or opac interface under plack with optional multi-process
    Starman server
    
      plackup.sh site-name [intranet]
    
    site-name is used to find config /etc/koha/sites/site-name/koha-conf.xml
    
    All configuration is specified in koha.psgi, which you are welcomed to edit
    and tune according to your development needs.
    
    When you are happy with it, rename it to site name and save it for safe-keeping.
    
    Test scenario:
    1. install plack and dependencies, using ./cpanm-install.pl
    
    2. start ./plackup.sh sitename
    
    3. open intranet page http://localhost:5001/cgi-bin/koha/mainpage.pl
       and verify that it works
    
    4. start ./plackup.sh sitename i[ntranet]
    
    5. open OPAC http://localhost:5000/cgi-bin/koha/opac-main.pl
       and verify that it works

--- misc/plack/plackup.sh
+++ misc/plack/plackup.sh
@@ -1,24 +1,51 @@
 #!/bin/sh -xe
 
-# --max-requests decreased from 1000 to 50 to keep memory usage sane
-# --workers 8 which is number of cores on machine
+# This is plack startup script for Koha
 
-site=ffzg
-test ! -z "$1" && site=$1 && shift
+# ./plackup.sh [site] [intranet]
+
+site=$1
+test ! -z "$site" && shift || site=srvgit # default
+
+export KOHA_CONF=/etc/koha/sites/$site/koha-conf.xml
+export LOGDIR="$( sudo -u $site-koha xmlstarlet sel -t -v 'yazgfs/config/logdir' $KOHA_CONF )"
+export INTRANETDIR="$( sudo -u $site-koha xmlstarlet sel -t -v 'yazgfs/config/intranetdir' $KOHA_CONF )"
+export OPACDIR="$( sudo -u $site-koha xmlstarlet sel -t -v 'yazgfs/config/opacdir' $KOHA_CONF | sed 's,/cgi-bin/opac,,' )"
+
 dir=`dirname $0`
 
 # enable memcache - it's safe even on installation which don't have it
 # since Koha has check on C4::Context
-export MEMCACHED_SERVERS=localhost:11211
+#export MEMCACHED_SERVERS=localhost:11211
+# pass site name as namespace to perl code
 export MEMCACHED_NAMESPACE=$site
-export MEMCACHED_DEBUG=1
+#export MEMCACHED_DEBUG=1
 
-export KOHA_CONF=/etc/koha/sites/$site/koha-conf.xml
-export OPACDIR="$( sudo -u $site-koha xmlstarlet sel -t -v 'yazgfs/config/opacdir' $KOHA_CONF | sed 's,/cgi-bin/opac,,' )"
-export LOGDIR="$( sudo -u $site-koha xmlstarlet sel -t -v 'yazgfs/config/logdir' $KOHA_CONF )"
+if [ ! -e "$INTRANETDIR/C4" ] ; then
+	echo "intranetdir in $KOHA_CONF doesn't point to Koha git checkout"
+	exit 1
+fi
+
+if [ -z "$1" ] ; then # type anything after site name for intranet!
+	INTRANET=1
+	PORT=5000
+else
+	PORT=5001
+	shift # pass rest of arguments to plackup
+fi
+
 
 # uncomment to enable logging
 #opt="$opt --access-log $LOGDIR/opac-access.log --error-log $LOGDIR/opac-error.log"
-opt="$opt --server Starman -M FindBin --max-requests 50 --workers 4 -E deployment"
-opt="$opt --reload -R $OPACDIR/../C4 -R $OPACDIR/../Koha"
-sudo -E -u $site-koha plackup -I $OPACDIR/.. $opt $* $dir/koha.psgi
+
+# --max-requests 50 decreased from 1000 to keep memory usage sane
+# --workers 4       number of cores on machine
+opt="$opt --server Starman -M FindBin --port $PORT --max-requests 50 --workers 4"
+
+# -E deployment     turn off access log on STDOUT
+#opt="-E deployment"
+
+# comment out reload in production!
+opt="$opt --reload -R $INTRANETDIR/C4 -R $INTRANETDIR/Koha"
+
+sudo -E -u $site-koha plackup -I $INTRANETDIR $opt $* $dir/koha.psgi
