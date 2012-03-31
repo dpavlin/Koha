9091c8d89c2d2d512143902a6c2ad9bfaf58b9bc
Dobrica Pavlinusic <dpavlin@rot13.org>
2012-03-31 02:09:07 +0200

    Bug 7844 - sudo with xmlstarlet

--- misc/plack/intranet-plack.sh
+++ misc/plack/intranet-plack.sh
@@ -5,7 +5,7 @@ test ! -z "$1" && site=$1
 dir=`dirname $0`
 
 export KOHA_CONF=/etc/koha/sites/$site/koha-conf.xml 
-export INTRANETDIR="$( xmlstarlet sel -t -v 'yazgfs/config/intranetdir' $KOHA_CONF )"
+export INTRANETDIR="$( sudo -u $site-koha xmlstarlet sel -t -v 'yazgfs/config/intranetdir' $KOHA_CONF )"
 
 if [ ! -e "$INTRANETDIR/C4" ] ; then
 	echo "intranetdir in $KOHA_CONF doesn't point to Koha git checkout"
