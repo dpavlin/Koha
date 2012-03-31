84ee99303f3d1f58142df4bd3776f0ef9765a16a
Dobrica Pavlinusic <dpavlin@rot13.org>
2012-04-01 00:12:38 +0200

    7863 test scenario

---
+++ misc/plack/test-scenario/7863-opac-addbybiblionumber.pl
@@ -0,0 +1,9 @@
+1. issue search query
+2. select 1 result
+3. add to: new list, list-1, private
+4. select 2 results
+5. add to (exiting) list-1
+4. select 3 results
+6. add to: new list, list-2, public
+7. using lists drop-down, view and delete list-1 and list-2
+
