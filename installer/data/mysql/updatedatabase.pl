5f53eda6f3b84701f21a87bc83aa6aed125b5bad
Dobrica Pavlinusic <dpavlin@rot13.org>
2012-04-05 14:15:30 +0200

    Bug 7857 - invalidate cached preferences after SetVersion
    This allows plack to correctly move to login page after database upgrade

--- installer/data/mysql/updatedatabase.pl
+++ installer/data/mysql/updatedatabase.pl
@@ -5388,6 +5388,7 @@ sub SetVersion {
       my $finish=$dbh->prepare("INSERT into systempreferences (variable,value,explanation) values ('Version',?,'The Koha database version. WARNING: Do not change this value manually, it is maintained by the webinstaller')");
       $finish->execute($kohaversion);
     }
+    C4::Context::clear_syspref_cache(); # invalidate cached preferences
 }
 exit;
 
