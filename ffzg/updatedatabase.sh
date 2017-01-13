45d6b129f8e0e93303807c831a4dc3dfcf085527
Dobrica Pavlinusic <dpavlin@rot13.org>
2017-01-13 10:53:38 +0100

    need to restart memcache to invalidate cache

--- ffzg/updatedatabase.sh
+++ ffzg/updatedatabase.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -xe
 
+sudo /etc/init.d/memcached restart
 ( time sudo -u ffzg-koha KOHA_CONF=/etc/koha/sites/ffzg/koha-conf.xml PERL5LIB=/srv/koha_ffzg ./installer/data/mysql/updatedatabase.pl 2>&1 ) | tee /tmp/update.log
