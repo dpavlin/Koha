dpavlin/Koha

commit 2bf3ce268de6bc8b2386cfb5d768f60b05d75f44

Bug 17196: [QA Follow-up] Additional fix on acqui/basketgroup
acqui/basketgroup calls GetOrders and expects marcxml in the results.
Fixing it by an additional call of GetXmlBiblio.

Signed-off-by: Marcel de Rooy <m.de.rooy@rijksmuseum.nl>

Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

acqui/basketgroup.pl
@@ -183,6 +183,7 @@ sub printbasketgrouppdf{
             # Editor Number
             my $en;
             my $edition;
+            my $ord->{marcxml} = C4::Biblio::GetXmlBiblio( $ord->{biblionumber} );
             my $marcrecord=eval{MARC::Record::new_from_xml( $ord->{marcxml},'UTF-8' )};
             if ($marcrecord){
                 if ( C4::Context->preference("marcflavour") eq 'UNIMARC' ) {

commit 4a6030888739281620aa6a6153f9a9ef2bb3310b

Bug 19332 - Basket grouping PDF and CSV exports empty
This bug was introduced in commit 2bf3ce268de6bc8b2386cfb5d768f60b05d75f44
Bug 17196: [QA Follow-up] Additional fix on acqui/basketgroup

acqui/basketgroup.pl
@@ -184,7 +184,7 @@ sub printbasketgrouppdf{
             # Editor Number
             my $en;
             my $edition;
-            my $ord->{marcxml} = C4::Biblio::GetXmlBiblio( $ord->{biblionumber} );
+            $ord->{marcxml} = C4::Biblio::GetXmlBiblio( $ord->{biblionumber} );
             my $marcrecord=eval{MARC::Record::new_from_xml( $ord->{marcxml},'UTF-8' )};
             if ($marcrecord){
                 if ( C4::Context->preference("marcflavour") eq 'UNIMARC' ) {