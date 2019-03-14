55d4b91071a4a48993d7b3ce35d3dea5bc75ef14
Dobrica Pavlinusic <dpavlin@rot13.org>
2019-03-14 19:32:21 +0100

    FFZG: restart plack after upgrade

--- ffzg/updatedatabase.sh
+++ ffzg/updatedatabase.sh
@@ -3,3 +3,4 @@
 sudo /etc/init.d/memcached restart
 ( time sudo -u ffzg-koha KOHA_CONF=/etc/koha/sites/ffzg/koha-conf.xml PERL5LIB=/srv/koha_ffzg ./installer/data/mysql/updatedatabase.pl 2>&1 ) | tee /tmp/update.log
 sudo /etc/init.d/memcached restart
+sudo koha-plack --restart ffzg
