e2720b782477c3cd8939e898c229c85415eca1a6
Dobrica Pavlinusic <dpavlin@rot13.org>
2019-03-26 18:22:56 +0100

    FFZG: ffzg/updatedatbase.sh generate yyyy-mm-dd.update.log file

--- ffzg/updatedatabase.sh
+++ ffzg/updatedatabase.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -xe
 
 sudo /etc/init.d/memcached restart
-( time sudo -u ffzg-koha KOHA_CONF=/etc/koha/sites/ffzg/koha-conf.xml PERL5LIB=/srv/koha_ffzg ./installer/data/mysql/updatedatabase.pl 2>&1 ) | tee /tmp/update.log
+( time sudo -u ffzg-koha KOHA_CONF=/etc/koha/sites/ffzg/koha-conf.xml PERL5LIB=/srv/koha_ffzg ./installer/data/mysql/updatedatabase.pl 2>&1 ) | tee `date +%Y-%m-%d`.update.log
 sudo /etc/init.d/memcached restart
 sudo koha-plack --restart ffzg
