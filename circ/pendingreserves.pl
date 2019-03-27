627199bf38c9b18b08f447c5ca6f431c2d7abbd8
Dobrica Pavlinusic <dpavlin@rot13.org>
2019-03-27 12:05:28 +0100

    FFZG ZS: notforloan visible for <= 0

--- circ/pendingreserves.pl
+++ circ/pendingreserves.pl
@@ -221,7 +221,7 @@ my $strsth =
     AND issues.itemnumber IS NULL
     AND reserves.priority <> 0 
     AND reserves.suspend = 0
-    AND notforloan >= 0 AND itemlost = 0 AND withdrawn = 0
+    AND notforloan <= 0 AND itemlost = 0 AND withdrawn = 0
     ";
     # GROUP BY reserves.biblionumber allows only items that are not checked out, else multiples occur when 
     #    multiple patrons have a hold on an item
